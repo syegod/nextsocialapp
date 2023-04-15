a5a8b073a4b70feab733fa6a66c3474e17ed716b
syegod <ovisochin362@gmail.com>
2023-04-15 14:49:32 +0200

    test

--- pages/api/profile/updateuseravatar.js
+++ pages/api/profile/updateuseravatar.js
@@ -10,19 +10,26 @@ cloudinary.config({
     secure: true
 })
 
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-}
 
 
 export default async function updateuseravatar(req, res) {
     try {
         const {aboba} = req.body
-        
-
-        return res.status(200).json({ message: 'Done!' })
+        // main()
+        // const form = formidable({ keepExtensions: true });
+        // form.parse(req, async (err, fields, files) => {
+        //     const result = await cloudinary.v2.uploader.upload(files.file.filepath,
+        //         {
+        //             public_id: fields.uid,
+        //             folder: 'nextfilms/avatars',
+        //             overwrite: true,
+        //             resource_type: 'image',
+        //         })
+        //     const user = await User.findOne({ _id: fields.uid })
+        //     user.avatar = result.secure_url
+        //     await user.save()
+        // })
+        return res.status(200).json({ message: aboba })
     } catch (e) {
         return res.status(500).json({ message: e.message || 'Something goes wrong!' })
     }
